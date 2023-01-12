5fc6f4259dd56ad0fe3a7a3ef810fad929f320b3
Johny-Code <jan.tobolewski09@gmail.com>
2023-01-12 13:47:54 +0100

    working pipeline for parking system

--- lala.py
+++ lala.py
@@ -6,6 +6,8 @@ from PIL import Image
 from pytesseract import pytesseract
 import os
 from matplotlib import pyplot as plt
+import pandas as pd
+import datetime
 
 def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
     # Resize and pad image while meeting stride-multiple constraints
@@ -48,115 +50,147 @@ data = './data/kaggle.yaml'
 dnn=False
 half=False
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 model = torch.hub.load('ultralytics/yolov5', 'custom', './runs/train/yolo_car_plates_test_set/weights/best.pt')
 
+df = pd.DataFrame(columns= ['id', 'date', 'path', 'car_photo_path', 'plate_img_path', 'licence_plate_number', 'anonymous_plate', 'anonymous_number'])
 
-img_path = './Dataset/Kaggle/images/test/Cars96.jpg'
+output_path_photos = os.path.join('.', 'results', 'cars')
+try:
+    os.makedirs(output_path_photos, exist_ok = True)
+    print(f"Directory {output_path_photos} created successfully")
+except OSError as error:
+    print(f"Directory {output_path_photos} can not be created")
 
-img = cv2.imread(img_path)
-results = model(img)
+output_path_plates = os.path.join('.', 'results', 'plates')
+try:
+    os.makedirs(output_path_plates, exist_ok = True)
+    print(f"Directory {output_path_plates} created successfully")
+except OSError as error:
+    print(f"Directory {output_path_plates} can not be created")
 
-boxes = results.pandas().xyxy[0].sort_values('xmin')
-# print(boxes)
+dataset_path = os.path.join('.', 'Dataset', 'Kaggle', 'images', 'test')
 
-confidence = 0
-for i in range(len(boxes)):
-    #bierzemy tylko tablicę z największą pewnością 
-    if boxes['confidence'][i] > confidence:
-        confidence = boxes['confidence'][i]
-        y_min = int(boxes['ymin'][i])
-        x_min = int(boxes['xmin'][i])
-
-        y_max = int(boxes['ymax'][i])
-        x_max = int(boxes['xmax'][i])
-
+id = 0
+for file in os.listdir(dataset_path):
+    if file.endswith(".jpg"):
+        img_path = os.path.join(dataset_path, file)
         
-color = (0, 0, 255)
-thickness = 2
+        img = cv2.imread(img_path)
 
-cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color, thickness)
+        #przepuszczanie przez YOLOv5 -> wyznaczenie położenia tablicy
+        results = model(img)
 
-# cv2.imshow('image', img)
-# cv2.waitKey()
+        boxes = results.pandas().xyxy[0].sort_values('xmin')
 
-#wycinanie tylko tablicy
-croped_plate = img[y_min:y_max, x_min:x_max]
+        #wyznaczenie gdzie jest najwięszka pewność, że tablica się znajduje (bo często są dwa albo 3 napisy na zdjęciach)
+        confidence = 0
+        for i in range(len(boxes)):
+            #bierzemy tylko tablicę z największą pewnością 
+            if boxes['confidence'][i] > confidence:
+                confidence = boxes['confidence'][i]
+                y_min = int(boxes['ymin'][i])
+                x_min = int(boxes['xmin'][i])
+                y_max = int(boxes['ymax'][i])
+                x_max = int(boxes['xmax'][i])
+        
+        car_img = img.copy()
+        cv2.rectangle(car_img, (x_min, y_min), (x_max, y_max), color = (0, 0, 255), thickness = 2)
+        car_image_path = os.path.join(output_path_photos,  (str(id) + '_car_photo_' + file))
+        cv2.imwrite(car_image_path, car_img)
+
+        #wycinanie tylko tablicy
+        croped_plate = img[y_min:y_max, x_min:x_max]
+
+        #przeskalowanie
+        scale_percent = 500 # o ile procent skalujemy - 500 %
+        width = int(croped_plate.shape[1] * scale_percent / 100)
+        height = int(croped_plate.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        croped_plate_resized = cv2.resize(croped_plate, dim, interpolation = cv2.INTER_AREA)
+        plate_img_path = os.path.join(output_path_plates,  (str(id) + '_plate_photo_' + file))
+        cv2.imwrite(plate_img_path, croped_plate_resized)
+
+        img_gray_lp = cv2.cvtColor(croped_plate_resized, cv2.COLOR_BGR2GRAY)                        #to gray scale
+        _, img_binary_lp = cv2.threshold(img_gray_lp, 200, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)  #convert to binary image
+        img_binary_lp = cv2.erode(img_binary_lp, (3,3))
+        img_binary_lp = cv2.dilate(img_binary_lp, (3,3))
+        
+        # TODO
+        #make borders white - jak starczy czasu
+
+        LP_WIDTH = img_binary_lp.shape[0]
+        LP_HEIGHT = img_binary_lp.shape[1]
+        # Estimations of character contours sizes of cropped license plates
+        lower_width = LP_WIDTH/6
+        upper_width = LP_WIDTH/2
+        lower_height = LP_HEIGHT/10
+        upper_height = 2*LP_HEIGHT/3
+
+        temp_img = img_binary_lp.copy()
+        ii = img_binary_lp.copy()
+
+        cntrs, _ = cv2.findContours(temp_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cntrs = sorted(cntrs, key=cv2.contourArea, reverse=True)[:15]
+
+        x_cntr_list = []
+        target_contours = []
+        img_res = []
+
+        for i, cntr in enumerate(cntrs) :
+            intX, intY, intWidth, intHeight = cv2.boundingRect(cntr)
+
+            # checking the dimensions of the contour to filter out the characters by contour's size
+            if intWidth > lower_width and intWidth < upper_width and intHeight > lower_height and intHeight < upper_height :
+                x_cntr_list.append(intX) #stores the x coordinate of the character's contour, to used later for indexing the contours
+
+                # extracting each character using the enclosing rectangle's coordinates.
+                char = temp_img[intY:intY+intHeight, intX:intX+intWidth]
+                char = cv2.resize(char, (20, 40))
+
+                cv2.rectangle(ii, (intX,intY), (intWidth+intX, intY+intHeight), (50,21,200), 2)
+
+                # ukrywanie co drugiego boxa
+                # if i % 2 == 0:
+                #     cv2.rectangle(ii, (intX,intY), (intWidth+intX, intY+intHeight), (50,21,200), -1)
+                
+                char = cv2.subtract(255, char)
+                char_copy = np.zeros((44,24))
+                char_copy[2:42, 2:22] = char
+                char_copy[0:2, :] = 0
+                char_copy[:, 0:2] = 0
+                char_copy[42:44, :] = 0
+                char_copy[:, 22:24] = 0
+
+                img_res.append(char_copy) # List that stores the character's binary image (unsorted)
+
+        cv2.imshow('Predicted segments', ii)
+        cv2.waitKey()
+
+        indices = sorted(range(len(x_cntr_list)), key=lambda k: x_cntr_list[k])
+        img_res_copy = []
+        for idx in indices:
+            img_res_copy.append(img_res[idx])# stores character images according to their index
+        img_res = np.array(img_res_copy)
+
+        #zczytanie akturalnej daty
+        ct = datetime.datetime.now()
+        #zapisanie danych do bazy danych
+        # (columns= ['id', 'date', 'path', 'car_photo_path', 'plate_img_path', 'licence_plate_number', 'anonymous_plate', 'anonymous_number'])
+        # parking_database_row = [id, ct, img_path, car_image_path, plate_img_path, ]
+        id += 1 
 
 
-#przeskalowanie
-scale_percent = 500 # percent of original size
-width = int(croped_plate.shape[1] * scale_percent / 100)
-height = int(croped_plate.shape[0] * scale_percent / 100)
-dim = (width, height)
-croped_plate_resized = cv2.resize(croped_plate, dim, interpolation = cv2.INTER_AREA)
 
-# filename = 'croped_plate.png'
-# cv2.imwrite(filename, croped_plate)
 
-img_gray_lp = cv2.cvtColor(croped_plate_resized, cv2.COLOR_BGR2GRAY)                        #to gray scale
-_, img_binary_lp = cv2.threshold(img_gray_lp, 200, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)  #convert to binary image
-img_binary_lp = cv2.erode(img_binary_lp, (3,3))
-img_binary_lp = cv2.dilate(img_binary_lp, (3,3))
 
-LP_WIDTH = img_binary_lp.shape[0]
-LP_HEIGHT = img_binary_lp.shape[1]
 
-# TODO
-#make borders white
 
-# Estimations of character contours sizes of cropped license plates
-lower_width = LP_WIDTH/6
-upper_width = LP_WIDTH/2
-lower_height = LP_HEIGHT/10
-upper_height = 2*LP_HEIGHT/3
+
 
 # cv2.imshow('croped', img_binary_lp)
 # cv2.waitKey()
 # cv2.imwrite('contour.jpg',img_binary_lp)
-temp_img = img_binary_lp.copy()
-ii = img_binary_lp.copy()
 
-cntrs, _ = cv2.findContours(temp_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cntrs = sorted(cntrs, key=cv2.contourArea, reverse=True)[:15]
-
-x_cntr_list = []
-target_contours = []
-img_res = []
-
-for i, cntr in enumerate(cntrs) :
-    intX, intY, intWidth, intHeight = cv2.boundingRect(cntr)
-
-    # checking the dimensions of the contour to filter out the characters by contour's size
-    if intWidth > lower_width and intWidth < upper_width and intHeight > lower_height and intHeight < upper_height :
-        x_cntr_list.append(intX) #stores the x coordinate of the character's contour, to used later for indexing the contours
-
-        char_copy = np.zeros((44,24))
-        # extracting each character using the enclosing rectangle's coordinates.
-        char = temp_img[intY:intY+intHeight, intX:intX+intWidth]
-        char = cv2.resize(char, (20, 40))
-
-        # temp usage
-        # path_to_tesseract = "C:/Program Files/Tesseract-OCR/tesseract.exe"
-        # pytesseract.tesseract_cmd = path_to_tesseract
-        # text = pytesseract.image_to_string(char, lang ='eng',
-        #     config ='--oem 3 --psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-        # print(f'rozpoznano {text}')
-
-        cv2.rectangle(ii, (intX,intY), (intWidth+intX, intY+intHeight), (50,21,200), 2)
-        if i % 2 == 0:
-            cv2.rectangle(ii, (intX,intY), (intWidth+intX, intY+intHeight), (50,21,200), -1)
-        
-        img_res.append(char_copy) # List that stores the character's binary image (unsorted)
-
-cv2.imshow('Predicted segments', ii)
-cv2.waitKey()
-
-indices = sorted(range(len(x_cntr_list)), key=lambda k: x_cntr_list[k])
-img_res_copy = []
-for idx in indices:
-    img_res_copy.append(img_res[idx])# stores character images according to their index
-img_res = np.array(img_res_copy)
 
 
 
